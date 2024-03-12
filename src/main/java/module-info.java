bc1260144f9eebb22e9b17453b51e9350a67f4c9
Javier Chaler Romero <javichr11@gmail.com>
2024-03-12 11:31:55 +0100

    creacion front

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module codebusters.smarttradefrontend {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens codebusters.smarttradefrontend to javafx.fxml;
+    exports codebusters.smarttradefrontend;
+}
