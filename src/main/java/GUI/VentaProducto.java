8850b4b627ee7b649540fcb60a2b9237ebbce7d4
Javier Chaler Romero <javichr11@gmail.com>
2024-04-08 19:01:04 +0200

    Añadido funcionalidad a VentaProducto

--- src/main/java/GUI/VentaProducto.java
+++ src/main/java/GUI/VentaProducto.java
@@ -1,11 +1,26 @@
 package GUI;
 
+import javafx.scene.control.ComboBox;
+
+import javax.accessibility.Accessible;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.NumberFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import javax.swing.plaf.basic.BasicComboBoxRenderer;
+import javax.swing.plaf.basic.ComboPopup;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
+import java.awt.event.KeyEvent;
 
-public class VentaProducto {
+public class VentaProducto extends JFrame{
 
     private JPanel panelProductoVenta;
     private JPanel panelTitulo;
@@ -20,6 +35,8 @@ public class VentaProducto {
     private JComboBox categorySelector;
     private JTextArea productDescription;
     private JButton validateProductButton;
+    private JLabel añade;
+    private JFrame frame;
 
     public VentaProducto(){
 
@@ -62,19 +79,26 @@ public class VentaProducto {
                 validateProductButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
                 validateProductButton.setBackground(new Color(72,231,255));
             }
+            @Override
             public void mouseExited(MouseEvent e) {
                 validateProductButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
                 validateProductButton.setBackground(new Color(153,233,255));
             }
+            @Override
+            public void mouseClicked(MouseEvent e){
+                String selection = getComboBox().getSelectedItem().toString();
+                if(acceptProduct()){
+                    JOptionPane.showMessageDialog(frame, "Producto válido, añadido al servicio", "Validación",JOptionPane.INFORMATION_MESSAGE);
+                }
+            }
         });
 
         /*CATEGORYSELECTOR*/
-        String[] categories = {"--Selecione categoria--", "Comida", "Ropa", "Juguete", "Indormática"};
+        String[] categories = {"--Selecione categoria--", "Comida", "Ropa", "Juguete", "Informática"};
         for(String opcion : categories){
             categorySelector.addItem(opcion);
         }
-        categorySelector.setRenderer(new DisabledFirstElementRenderer());
-        //categorySelector.setModel(new DisabledFirstElementComboBoxModel(categories));
+
         categorySelector.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -85,21 +109,35 @@ public class VentaProducto {
                 categorySelector.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
             }
         });
-        categorySelector.addMouseMotionListener(new MouseMotionAdapter() {
+        Component comp = categorySelector.getComponent(0);
+        if(comp instanceof JButton){
+            JButton arrow = (JButton)comp;
+            arrow.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        }
+        categorySelector.addPopupMenuListener(new PopupMenuListener() {
             @Override
-            public void mouseMoved(MouseEvent e) {
-                int index = categorySelector.getSelectedIndex();
-                if (index != -1) {
-                    categorySelector.setCursor(new Cursor(Cursor.HAND_CURSOR));
-                } else {
-                    categorySelector.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+                JComboBox<?> comboBox = (JComboBox<?>) e.getSource();
+                Accessible a = comboBox.getUI().getAccessibleChild(comboBox, 0);
+                if (a instanceof ComboPopup) {
+                    JList<?> list = ((ComboPopup) a).getList();
+                        list.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                 }
             }
+
+            @Override
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+                JComboBox<?> comboBox = (JComboBox<?>) e.getSource();
+                comboBox.setCursor(Cursor.getDefaultCursor());
+            }
+
+            @Override
+            public void popupMenuCanceled(PopupMenuEvent e) {
+                JComboBox<?> comboBox = (JComboBox<?>) e.getSource();
+                comboBox.setCursor(Cursor.getDefaultCursor());
+            }
         });
-
         /*PERFILBUTTON*/
-
-
         perfilButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
@@ -122,6 +160,33 @@ public class VentaProducto {
                 }
             }
         });
+        ((AbstractDocument) productPriceTF.getDocument()).setDocumentFilter(new DocumentFilter() {
+            @Override
+            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+                // Verificar si la cadena que se está insertando es válida
+                if (isValidInsertion(fb.getDocument(), offset, string)) {
+                    super.insertString(fb, offset, string, attr);
+                }
+            }
+
+            @Override
+            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                // Verificar si el texto que se está reemplazando es válido
+                if (isValidInsertion(fb.getDocument(), offset, text)) {
+                    super.replace(fb, offset, length, text, attrs);
+                }
+            }
+
+            private boolean isValidInsertion(Document document, int offset, String text) throws BadLocationException {
+                String currentText = document.getText(0, document.getLength());
+                StringBuilder newText = new StringBuilder(currentText);
+                newText.insert(offset, text);
+
+                // Verificar si el nuevo texto cumple con el formato de precio válido
+                return isValidPrice(newText.toString());
+            }
+        });
+
 
         /*IMGBUTTON*/
 
@@ -136,10 +201,11 @@ public class VentaProducto {
         });
 
         /*PRODUCTDESCRIPTION*/
+
         productDescription.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                if(productDescription.getText().length() > 150){
+                if(productDescription.getText().length() > 350){
                     e.consume();
                 }
             }
@@ -164,10 +230,41 @@ public class VentaProducto {
         public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
             JLabel component = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
             if (index == 0) {
-                component.setEnabled(false);
+
             }
             return component;
         }
+    }
+    public JComboBox getComboBox(){
+        return this.categorySelector;
+    }
+    public boolean validationComboBox(String selection){
+        String res = getComboBox().getItemAt(0).toString();
+        return selection.equals(res);
+    }
+    public boolean isValidPrice(String text) {
+            return text.matches("^\\d+(\\.\\d{0,2})?$");
+    }
+    public boolean isValidName(String name){
+        return name.matches("[a-zA-Z]+");
+    }
+
+    public boolean acceptProduct(){
+
+        if(!validationComboBox(categorySelector.getSelectedItem().toString())){
+            if(isValidName(productNameTF.getText())){
+                if(isValidPrice(productPriceTF.getText())){
+                    return true;
+                }else{
+                    JOptionPane.showMessageDialog(frame, "Precio con formato incorrecto", "Error",JOptionPane.ERROR_MESSAGE);
+                }
+            }else{
+                JOptionPane.showMessageDialog(frame, "Nombre no válido", "Error",JOptionPane.ERROR_MESSAGE);
+            }
+        }else{
+            JOptionPane.showMessageDialog(frame, "Categoria seleccionada no válida", "Error",JOptionPane.ERROR_MESSAGE);
+        }
+    return false;
     }
 
 }
