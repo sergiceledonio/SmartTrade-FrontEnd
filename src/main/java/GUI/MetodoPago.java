d17e8d384c6163c8514cf70dd96b41e6606e9092
IvanCrespoG <icregad@etsinf.upv.es>
2024-05-24 11:43:58 +0200

    interfaz pago

--- src/main/java/GUI/MetodoPago.java
+++ src/main/java/GUI/MetodoPago.java
@@ -49,9 +49,62 @@ public class MetodoPago {
                 backButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
             }
         });
+
+        addPaypalButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                goToPaypal();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                backButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                backButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
+
+        addTrajetaButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                goToCard();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                backButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                backButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
     }
     public void backMenu(){
         CatalogoProductos ventanaCatalog = new CatalogoProductos(getUserData(), tipo, id);
+        JFrame ventanaAtras = new JFrame("Smart Trade");
+        ventanaAtras.setContentPane(ventanaCatalog.getPanel());
+        ventanaAtras.pack();
+        ventanaAtras.setVisible(true);
+        JFrame ventanaActual = (JFrame) SwingUtilities.getWindowAncestor(getPanel());
+        ventanaActual.dispose();
+    }
+    public void goToPaypal(){
+        PagoPaypal ventanaCatalog = new PagoPaypal(tipo, id, precio);
+        JFrame ventanaAtras = new JFrame("Smart Trade");
+        ventanaAtras.setContentPane(ventanaCatalog.getPanel());
+        ventanaAtras.pack();
+        ventanaAtras.setVisible(true);
+        JFrame ventanaActual = (JFrame) SwingUtilities.getWindowAncestor(getPanel());
+        ventanaActual.dispose();
+    }
+
+    public void goToCard(){
+        PagoTarjeta ventanaCatalog = new PagoTarjeta(tipo, id, precio);
         JFrame ventanaAtras = new JFrame("Smart Trade");
         ventanaAtras.setContentPane(ventanaCatalog.getPanel());
         ventanaAtras.pack();
--- src/main/java/GUI/PagoPaypal.java
+++ src/main/java/GUI/PagoPaypal.java
@@ -1,6 +1,11 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import static GUI.CatalogoProductos.getUserData;
 
 public class PagoPaypal {
     private JPanel panelTitulo;
@@ -10,4 +15,51 @@ public class PagoPaypal {
     private JPasswordField passwordField;
     private JButton pagarButton;
     private JCheckBox guardaCheckBox;
+    private JLabel precioLabel;
+    private JPanel paypalPanel;
+    private InicioSesion iniciosesion;
+    private int tipo;
+    private int id;
+    private double precio;
+
+    public PagoPaypal(int t, int id, double precio) {
+        iniciosesion = new InicioSesion();
+        paypalPanel.setPreferredSize(new Dimension(800, 600));
+        this.tipo = t;
+        this.id = id;
+        this.precio = precio;
+        precioLabel.setText("Precio final: " + precio + " €");
+        paypalPanel.setFocusable(true);
+        paypalPanel.requestFocusInWindow();
+
+        backButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                backMenu();
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                backButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                backButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+        });
+    }
+
+    public void backMenu(){
+        MetodoPago ventanaCatalog = new MetodoPago(tipo, id, precio);
+        JFrame ventanaAtras = new JFrame("Smart Trade");
+        ventanaAtras.setContentPane(ventanaCatalog.getPanel());
+        ventanaAtras.pack();
+        ventanaAtras.setVisible(true);
+        JFrame ventanaActual = (JFrame) SwingUtilities.getWindowAncestor(getPanel());
+        ventanaActual.dispose();
+    }
+    public JPanel getPanel(){
+        return paypalPanel;
+    }
 }
